fb2314f5efaf5a85f41c83eb2d5644bf25203de0
1337DS <77491801+1337DS@users.noreply.github.com>
2022-01-21 02:02:06 +0100

    ein push

--- flask/flaskapp.py
+++ flask/flaskapp.py
@@ -14,6 +14,8 @@ import RPi.GPIO as GPIO
 from flask import Flask, render_template, request
 import time
 import os
+from datetime import datetime
+import re
 app = Flask(__name__)
 
 # Setzen der GPIO Ein-/Ausgaenge
@@ -24,7 +26,7 @@ GPIO.setup(21, GPIO.OUT)
 
 GPIO.setup(20, GPIO.IN)
 
-
+lf = "/home/pi/garage/logs.txt"
 
 @app.route("/")
 def main():
@@ -40,14 +42,39 @@ def action():
     GPIO.output(21, GPIO.HIGH)
     time.sleep(1)
     GPIO.output(21, GPIO.LOW)
+    
+    
+# Eintrag ins Logfile
+    log_file = open(lf, "a")
+
+    #read logfile
+    with open(lf) as file:
+        lines = file.readlines()
+        logs = [line.rstrip() for line in lines]
+    
+    if len(logs) > 0:
+        log = open(lf, "r")
+        lines = log.readlines()
+        last_line = lines[-1]
+        ts="01.01.2022"
+        # @ Luca debugging necessary here        
+        #ts = datetime.strptime(re.findall(".+?(?= ->)", last_line)[0], '%d/%m/%y %H:%M:%S')
+    #if kennzeichen in whitelist dann öffne
+        ts = datetime.now().strftime('%d/%m/%y %H:%M:%S')
+        print("Garage-door opened for Web-App")
+        log_file.write("{ts}; Website\n") 
+    
+    
+    
+    
     return render_template('main.html')
 
 @app.route('/logs')
 def info():
-      with open("/home/pi/garage/logs.txt") as f:
+      with open(lf) as f:
         logdata = f.readlines()
         return render_template('logs.html',logdata=logdata)
-        
+      
       
     
 if __name__ == "__main__":
